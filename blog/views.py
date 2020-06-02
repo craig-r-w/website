3c34da0eded5f93d368d6b4fda536785fec58cb6
craig <craigwilbourne@zoho.com>
2020-06-02 17:31:29 +0100

    Added ability to display Posts as multiple pages.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 from .models import Post
+
+from django.core.paginator import Paginator
 
 # The home view.
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
-    return render(request, 'blog/display_posts.html', {'posts': posts})
+    paginator = Paginator(posts, 3) # Show 3 posts per page.
+
+    # GET the specified page number.
+    # e.g. ?page=1
+    page_number = request.GET.get('page')
+
+    # Select the relevant page.
+    page = paginator.get_page(page_number)
+
+    return render(request, 'blog/display_posts.html', {'posts': posts, 'page': page})
