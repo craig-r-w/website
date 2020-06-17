5be0e55e7c267eaeaac21de506fdb8d233e8b26e
craig <craigwilbourne@zoho.com>
2020-06-17 12:50:30 +0100

    Added isDebug to streamline production.

--- base/settings.py
+++ base/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .secrets import getSecretKey
+from .secrets import getSecretKey, isDebug
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = getSecretKey()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = isDebug()
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
